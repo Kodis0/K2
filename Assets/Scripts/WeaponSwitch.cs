794aa4edb690d0be8c3a47ba31cba2215d449b30
Agapie <laikaking@yandex.ru>
2023-06-03 07:25:25 +0300

    Меч добавил bruh

---
+++ Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    public GameObject katana;
+    public GameObject grappleGun;
+    public GameObject player;
+    void Update()
+    {
+        UseWeapon();
+    }
+    public void UseWeapon()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            player.GetComponent<Katana>().enabled = true;
+            player.GetComponent<Grappling>().enabled = false;
+            player.GetComponent<SwingingDone>().enabled = false;
+            Animator anim = katana.GetComponent<Animator>();
+            anim.SetBool("Ready", true);
+            grappleGun.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            player.GetComponent<Katana>().enabled = false;
+            player.GetComponent<Grappling>().enabled = true;
+            player.GetComponent<SwingingDone>().enabled = true;
+            Animator anim = katana.GetComponent<Animator>();
+            anim.SetBool("Ready", false);
+            grappleGun.SetActive(true);
+        }
+    }
+}
