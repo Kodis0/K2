0f00af01daffbc7bb58b7a00689719a84914e9df
Agapie <125921722+agap1e@users.noreply.github.com>
2023-06-13 16:33:32 +0300

    всё

---
+++ Assets/Scripts/StateAttack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class StateAttack : StateMachineBehaviour
+{
+    Transform player;
+    public float AttackDist = 3.5f;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+        if (distance > AttackDist)
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Yasher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yasher : MonoBehaviour
+{
+    public void die()
+    {
+        Destroy(this.gameObject);
+    }
+}
